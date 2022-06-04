3158da0091dde44620fd5c49aa7089368415a02b
Naser Rafinia <Naser.r@gmail.com>
2022-06-04 13:20:49 +0430

    Added support for ISO 639-1

--- src/Emby.Subtitle.Subscene/Providers/SubsceneSubtitleProvider.cs
+++ src/Emby.Subtitle.Subscene/Providers/SubsceneSubtitleProvider.cs
@@ -259,111 +259,143 @@ namespace Emby.Subtitle.Subscene.Providers
                 case "per":
                     lang = "farsi_persian";
                     break;
+                case "ar":
                 case "ara":
                     lang = "arabic";
                     break;
+                case "en":
                 case "eng":
                     lang = "english";
                     break;
+                case "my":
                 case "bur":
                     lang = "burmese";
                     break;
+                case "da":
                 case "dan":
                     lang = "danish";
                     break;
+                case "nl":
                 case "dut":
                     lang = "dutch";
                     break;
+                case "he":
                 case "heb":
                     lang = "hebrew";
                     break;
+                case "id":
                 case "ind":
                     lang = "indonesian";
                     break;
+                case "ko":
                 case "kor":
                     lang = "korean";
                     break;
+                case "ms":
                 case "may":
                     lang = "malay";
                     break;
+                case "es":
                 case "spa":
                     lang = "spanish";
                     break;
+                case "vi":
                 case "vie":
                     lang = "vietnamese";
                     break;
+                case "tr":
                 case "tur":
                     lang = "turkish";
                     break;
+                case "bn":
                 case "ben":
                     lang = "bengali";
                     break;
+                case "bg":
                 case "bul":
                     lang = "bulgarian";
                     break;
+                case "hr":
                 case "hrv":
                     lang = "croatian";
                     break;
+                case "fi":
                 case "fin":
                     lang = "finnish";
                     break;
+                case "fr":
                 case "fre":
                     lang = "french";
                     break;
+                case "de":
                 case "ger":
                     lang = "german";
                     break;
+                case "el":
                 case "gre":
                     lang = "greek";
                     break;
+                case "hu":
                 case "hun":
                     lang = "hungarian";
                     break;
+                case "it":
                 case "ita":
                     lang = "italian";
                     break;
+                case "ku":
                 case "kur":
                     lang = "kurdish";
                     break;
+                case "mk":
                 case "mac":
                     lang = "macedonian";
                     break;
+                case "ml":
                 case "mal":
                     lang = "malayalam";
                     break;
+                case "nn":
                 case "nno":
-                    lang = "norwegian";
-                    break;
+                case "nb":
                 case "nob":
-                    lang = "norwegian";
-                    break;
+                case "no":
                 case "nor":
                     lang = "norwegian";
                     break;
+                case "pt":
                 case "por":
                     lang = "portuguese";
                     break;
+                case "ru":
                 case "rus":
                     lang = "russian";
                     break;
+                case "sr":
                 case "srp":
                     lang = "serbian";
                     break;
+                case "si":
                 case "sin":
                     lang = "sinhala";
                     break;
+                case "sl":
                 case "slv":
                     lang = "slovenian";
                     break;
+                case "sv":
                 case "swe":
                     lang = "swedish";
                     break;
+                case "th":
                 case "tha":
                     lang = "thai";
                     break;
+                case "ur":
                 case "urd":
                     lang = "urdu";
                     break;
+                case "pt-br":
                 case "pob":
                     lang = "brazillian-portuguese";
                     break;
